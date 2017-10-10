2be0f4378f57326584d4590e3bc4206755f008e1
Christopher Miller <camiller.yr@gmail.com>
2017-10-10 12:37:57 -0500

    Added elements from a database in localStorage to DOM using document methods using for / for in loops

--- inventory.js
+++ inventory.js
@@ -1,1 +1,21 @@
 const HomeInventory = JSON.parse(localStorage.getItem("homeInventory"))
+
+const HomeInventoryElement = document.getElementsByClassName("inventory")[0]
+
+for (var key in HomeInventory) {
+    const HomeInventoryType = HomeInventory[key]
+
+    for (var index = 0; index < HomeInventoryType.length; index++) {
+        // console.log(HomeInventoryType[index]);
+        HomeInventoryItem = HomeInventoryType[index];
+        HomeInventoryElement.innerHTML += `
+        <div class="${HomeInventoryItem.type} ${(HomeInventoryItem.location).replace(/ /, "_")} HomeInventory">
+        <h2>${HomeInventoryItem.name}</h2>
+        <p>${HomeInventoryItem.description}</p>
+        </div>
+        `
+        
+    }
+}
+
+    
